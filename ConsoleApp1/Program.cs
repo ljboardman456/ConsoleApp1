7b654f61d920fef5b9628fc51df12113c6bebc97
levi boardman <leviboardman456@gmail.com>
2025-01-21 13:21:49 -0600

    commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //print prompt for width
+            Console.WriteLine("What is the width?");
+            //collect width input
+            double width = Convert.ToDouble(Console.ReadLine());
+            //print prompt for height
+            Console.WriteLine("What is the height?");
+            //collect height input
+            double height = Convert.ToDouble(Console.ReadLine());
+            //collect area
+            double area = width * height;   
+            //collect perimeter
+            double perimeter = (width+height)/2;
+            //print result area
+            Console.WriteLine("Area: " + area);
+            //print result perimeter
+            Console.WriteLine("Perimeter: " + perimeter);
+            //wait for user input
+            Console.ReadLine();
         }
     }
 }
